8db70d67b2e1542fc8a37150a0c5b854b069cab7
dhackelman <drew.hackelman@gmail.com>
2017-02-09 15:37:39 -0500

    Successful data retrieval loops

--- public/js/app.js
+++ public/js/app.js
@@ -2,16 +2,33 @@
   "use strict"
   const brewModule = function () {
     const searchBar = document.querySelector('.search-nav');
+    const breweryButton = document.querySelector('.show-breweries');
 
     function bindEvents() {
-      getSearchValue();
+      // getSearchValue();
+      showBreweries();
     }
 
-    function getSearchValue() {
-      searchBar.addEventListener('submit', () => {
-        event.preventDefault();
-        console.log(event.target[0].value);
-      })
+    function displayBreweries() {
+      $.get('./data/beer-objects.json').then((response) => {
+        console.log(response);
+      }).catch((error) => {
+        console.log(error);
+      });
+    }
+
+    // function getSearchValue() {
+    //   searchBar.addEventListener('submit', () => {
+    //     event.preventDefault();
+    //     console.log(event.target[0].value);
+    //   })
+    // }
+
+    function showBreweries() {
+      breweryButton.addEventListener('click', () => {
+          event.preventDefault();
+          displayBreweries();
+        });
     }
 
     function init() {
